165fb0544aeee7f27b91caac501d7e15939fbdf0
ayon <21101032@uap-bd.edu>
2022-07-12 12:19:46 +0600

    Initial commit

---
+++ sheet.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lld long long int
+#define ui unsigned int
+int main()
+
+{
+ios_base ::sync_with_stdio(0);
+cin.tie(0);
+
+cout<<"ayon"<<endl;
+return 0;
+}
